7f172f81074b78c6eb39de4b9921eb219421fe70
Cleverson Santos <cleverson.santos@deal.com.br>
2019-03-19 12:45:49 -0300

    Application configurations changed.

--- src/main/java/beer/me/bestbeer/BestBeerApplication.java
+++ src/main/java/beer/me/bestbeer/BestBeerApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BestBeerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BestBeerApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(BestBeerApplication.class, args);
+  }
 }
