32ba9cb1053a388e8e28971f73534067dac5ba9e
Cleverson Santos <cleverson.santos@deal.com.br>
2019-03-19 12:19:27 -0300

    Init

---
+++ src/main/java/beer/me/bestbeer/BestBeerApplication.java
@@ -0,0 +1,13 @@
+package beer.me.bestbeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestBeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestBeerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/beer/me/bestbeer/BestBeerApplicationTests.java
@@ -0,0 +1,16 @@
+package beer.me.bestbeer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BestBeerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
